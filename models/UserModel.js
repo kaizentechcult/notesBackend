56641b8bb69f4a6a8d8d6ac001bd2a4ff21688d5
krishkh <krishkhanna00@gmail.com>
2024-09-16 23:27:31 +0530

    added backend

---
+++ models/UserModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", UserSchema);
+export default User;
